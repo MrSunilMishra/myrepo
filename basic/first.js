2dc601013e5328f7246e2fa63b83f718ac0c019f
Mr Sunil Mishra <162742450+MrSunilMishra@users.noreply.github.com>
2025-09-22 10:41:16 +0000

    taste let

--- basic/first.js
+++ basic/first.js
@@ -1,5 +1,2 @@
-console.log('Hello, World!');
-console.log('This is a basic JavaScript file.');
-console.log('It demonstrates simple logging to the console.');
-console.log('You can run this file using Node.js or in a web browser console.');
-console.log('Feel free to modify and experiment with the code!');
+let message = "Hello, World!";
+console.log(message);
