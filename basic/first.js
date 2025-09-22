517627c4ca7f39a0f5dfcae0b6f3fa2a37fc33e5
Mr Sunil Mishra <162742450+MrSunilMishra@users.noreply.github.com>
2025-09-22 10:38:33 +0000

    add new console

--- basic/first.js
+++ basic/first.js
@@ -1,1 +1,5 @@
 console.log('Hello, World!');
+console.log('This is a basic JavaScript file.');
+console.log('It demonstrates simple logging to the console.');
+console.log('You can run this file using Node.js or in a web browser console.');
+console.log('Feel free to modify and experiment with the code!');
