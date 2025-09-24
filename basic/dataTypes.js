b74ac54848a1830d7b6fc3cee23f5b947ab4687b
Mr Sunil Mishra <162742450+MrSunilMishra@users.noreply.github.com>
2025-09-24 15:59:58 +0000

    dta types

---
+++ basic/dataTypes.js
@@ -0,0 +1,39 @@
+// "use strict"; // treat all JS code as newer versions
+
+// alert("Hello World!"); we are using node.js, not a browser
+
+// console.log("Hello World!");
+
+// number => 1, 
+// string => "hello", 'hello', `hello`
+// boolean => true, false
+// null => standalone value
+// undefined => standalone value
+// object => { key: value }
+// symbol => unique identifier
+
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+let age = null;
+console.log(age, typeof age);
+
+age = 22;
+console.log(age, typeof age);
+
+let name = "John";
+console.log(name, typeof name);
+
+let isApproved = false; // true
+console.log(isApproved, typeof isApproved);
+
+let firstName; // undefined
+console.log(firstName, typeof firstName);
+
+firstName = "Jane";
+console.log(firstName, typeof firstName);
+
+
+
